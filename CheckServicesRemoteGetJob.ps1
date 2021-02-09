efef33334c27732442946a6dbb730a332dd4f4ab
Drumsand <53863916+Drumsand@users.noreply.github.com>
2021-02-09 19:50:19 +0100

    Update CheckServicesRemoteGetJob.ps1

--- CheckServicesRemoteGetJob.ps1
+++ CheckServicesRemoteGetJob.ps1
@@ -64,14 +64,14 @@ $ReportHTMLFile = "${Path}\_LOG\status_service_html_${CurrentDateLog}.html"
 #variable for Select-Object
 $SelectColumns = 'PSComputerName, Name, StartName, StartMode, State'
 # variable for logo file
-$Logo = '<img style=vertical-align:middle; src="\\Server_Name\PS_Script\CheckServicesRemoteGetJob\Demant_Logo.png" alt="Demant_logo">'
+$Logo = '<img style=vertical-align:middle; src="\\Server_Name\PS_Script\CheckServicesRemoteGetJob\Logo.png" alt="logo">'
 
 
 # CSS style for logfile (HTML UTF8)
 $css = @"
 <style>
 body 	{ background: #000; color: #E3E3E3; }
-table   { table-layout: fixed; width: 98%; margin: auto; font-family: Segoe UI; box-shadow: 10px 10px 5px #D3D3D3; border: thin ridge grey; color: #DDD; }
+table   { table-layout: fixed; width: 98%; margin: auto; font-family: Segoe UI; border: color: #DDD; }
 h1      { text-align: left; font-size: 12px; text-align: left; font-family: Segoe UI; display: table-cell; vertical-align: middle; }
 h5		{ text-align: left; font-size: 12px; text-align: left; font-family: Segoe UI;}
 th      { text-align: left; font-size: 12px; text-align: left; font-family: Segoe UI; background: #333333; color: #E6E8E9; max-width: 400px; padding: 3px 10px; position: sticky; top: 0px; }
@@ -81,7 +81,7 @@ tr:nth-child(even)  { background: #2E5266; color: #B3B3B3; }
 tr:nth-child(odd)   { background: #6E8898; color: #E3E3E3; }
 
 .col-sn-width       { width: 120px; }       /* ServerName column width */
-.col-ut-width       { width: 100px; }       /* UpTime column width */
+.col-ut-width       { width: 90px; }        /* UpTime column width */
 .col-ss-width       { width: 150px; }       /* Service Status column width */
 .col-ds-width       { width: 80px; }        /* Delayed Start column width */
 .col-sm-width       { width: 80px; }        /* Start Mode column width */
@@ -124,98 +124,17 @@ tr:nth-child(odd)   { background: #6E8898; color: #E3E3E3; }
 # .Disabled           { color:#FE2020; background: #FFE400 }
 # .Manual             { color:#66FCF1; background: #272727 }
 
-# Additional CSS to highlight status of a service in logfile
-$StatusColor = @{ `
-    'Continue Pending'     = ' class="Continue-Pending">Continue Pending<'; # The service has been paused and is about to continue.
-    Paused                 = ' class="Paused">Paused<';                     # The service is paused.
-    'Pause Pending'        = ' class="Pause-Pending ">Pause Pending<';      # The service is in the process of pausing.
-    Running                = ' class="Running">Running<';                   # The service is running.
-    'Start Pending'        = ' class="blink-bg">Starting<';                 # The service is in the process of starting.
-    Stopped                = ' class="Stopped">Stopped<';                   # The service is not running.
-    'Stop Pending'         = ' class="Stopping">Stopping<';                 # The service is in the process of stopping.
-    Disabled               = ' class="Disabled">Disabled<';                 # The startup mode is set to DISABLED
-    Manual                 = ' class="Manual">Manual<';                     # The startup mode is set to MANUAL
-}
-
-# server list to check from file (CSV)
-#
-# $DNSList = @(Get-Content ${Path}\Ping_Pong\Ping_Pong_ENV_TEST.csv)
-$DNSList = @(Get-Content "${Path}\Ping_Pong\$($EnvName).txt")
-# $DNSList = @( "servername" )
-
-$s = New-PSSession -ComputerName $DNSList -Name Marty-CheckService
-
-$Jobs = Invoke-Command -Session $s {
-    $Boot = (Get-CimInstance -ClassName win32_operatingsystem | Select-Object LastBootUpTime).lastbootuptime
-    $Today = Get-Date
-    $UpTime = New-TimeSpan -start $Boot -end $Today
-    $CimOSUpTime = "Uptime: " + $UpTime.Days + "d " + $UpTime.Hours + "h "
-
-    Get-CimInstance -class win32_service |
-        Where-Object { (
-                $_.Name -match "AOS60"                  -or
-                $_.Name -match "ATLAS_"                 -or         <# POS Services #>
-                $_.Name -match "MSSQL"                  -or
-                $_.Name -match "SQLAgent"               -or
-                $_.Name -match "SQLSERVERAGENT"         -or
-                $_.Name -match "ReportServer"           -or
-                $_.Name -match "Lasernet"               -or
-                $_.Name -match "IISADMIN"               -or         <# IIS #>
-                $_.Name -match "W3SVC"                  -or         <# IIS / World Wide Web Publishing Service #>
-                $_.Name -match "WMSVC"                  -or         <# Web Management Service #>
-                $_.Name -match "CRM"                    -or
-                $_.Name -match "MSCRM"                  -or
-                ( $_.Name -match "spool" | Where-Object { ( $_.PSComputerName -like "*OMS*" ) } )  -or
-                $_.DisplayName -match "Dynamics 365"    -or
-                $_.DisplayName -match "Mongo"           -and
-                !($_.Name -match "MSSQLFDLauncher")                 <# non-essential SQL #>
-                # $_.Name -match "RSoPProv" `             -or         <# The one with manual startup (test) #>
-                # $_.Name -match "tzautoupdate" `         -or         <# The one with disabled startup (test) #>
-            ) } |
-        Select-Object -Property *, @{Name = 'Server UpTime'; Expression = { $CimOSUpTime } }
-} -AsJob -JobName Marty!
-
-# # Get all the running jobs
-# $jobs = get-job | ? { $_.state -eq "running" }
-# $total = $jobs.count
-# $runningjobs = $jobs.count
-
-
-# Jobs progress simple no info
-Write-Host " `nJobs running " -ForegroundColor DarkCyan
-while (($Jobs.State -eq "Running") -and ($Jobs.State -ne "NotStarted")) {
-    Write-Host '.' -NoNewline -ForegroundColor DarkCyan
-    Start-Sleep -Seconds 1
-
-}
-
-# New line for nice output
-Write-Host "`n`t"
-
-# receive job to list services status | sort | export logfile (CSV UTF8)
-Get-Job | Wait-Job | Receive-Job -ErrorAction SilentlyContinue |
-    Sort-Object PSComputerName, Service |
-    Export-Csv -Path $ReportDataFile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
-
-
-# Kill opened PS Sessions
-Write-Host "Removing Powershell sessions from remote computers"
-Remove-PSSession $s # -Verbose
-# $s | % { Remove-PSSession -Session $s } -Verbose
-
-# Create logfile (HTML UTF8)
-# ---
-
-
 # Values for log. Services
 $GService = Import-Csv $ReportDataFile -Delimiter ";" |
-    Select-Object `
-        @{Name = "Server Name"; Expression = { $_.PSComputerName } },
-        @{Name = "Service"; Expression = { $_.Name } },
-        @{Name = "Logon Account"; Expression = { $_.StartName } },
-        @{Name = "Service Start Mode"; Expression = { $_.StartMode } },
-        @{Name = "Service Status"; Expression = { $_.State } } |
-    ConvertTo-HTML -AS Table -Fragment -PreContent '<table><colgroup><col/></colgroup><tr><th><h1>Services on Servers</h1></th></tr>' | Out-String
+    Select-Object (
+        @{ n = "Server Name";        e = { $_.PSComputerName } },
+        'Server UpTime',
+        @{ n = "Service";            e = { $_.Name } },
+        @{ n = "Service Status";     e = { $_.State } },
+        @{ n = "Start Mode";         e = { $_.StartMode } },
+        @{ n = "Logon Account";      e = { $_.StartName } },
+        @{ n = "Delayed Start";      e = { $_.DelayedAutoStart } }
+    ) | ConvertTo-HTML -AS Table -Fragment -PreContent '<table><colgroup><col/></colgroup><tr><th><h1>Services on Servers</h1></th></tr>' | Out-String
 
 # Logfile (HTML UTF8) status color
 $StatusColor.Keys | ForEach-Object { $GService = $GService -replace ">$_<", ($StatusColor.$_) }
@@ -239,7 +158,8 @@ $Check2 = (Import-Csv $ReportDataFile -Delimiter ";").PSComputerName | Sort-Obje
 # $sb
 
 
-$GError = Import-Csv $ReportErrorFile -Delimiter ";" | ConvertTo-Html -AS Table -Fragment -PreContent '<table><colgroup><col/></colgroup><tr><th><h1>Servers not checked </h1></th></tr>' | Out-String
+$GError = Import-Csv $ReportErrorFile -Delimiter ";" |
+    ConvertTo-Html -AS Table -Fragment -PreContent '<table><colgroup><col/></colgroup><tr><th><h1>Servers not checked </h1></th></tr>' | Out-String
 
 
 #
@@ -275,12 +195,12 @@ Write-Host " $($ReportHTMLFile) " -BackgroundColor DarkCyan -ForegroundColor Whi
 #Write-Host " Servers NOT checked " -BackgroundColor Red -ForegroundColor Black
 #Write-Host "$($ListCompare)" -BackgroundColor Black -ForegroundColor Red
 
-# Replace column names in HTML
-#(Get-Content $ReportHTMLFile).replace("PSComputerName", "Server Name").Replace("Name", "Service Name") | `
-#Set-Content $ReportHTMLFile
+# Replace details in HTML
+(Get-Content $ReportHTMLFile).replace('GreenClock',' &#128338; ') | Set-Content $ReportHTMLFile
+(Get-Content $ReportHTMLFile).replace('RedClock', ' &#x1F534; ') | Set-Content $ReportHTMLFile
 
 # open logfile (HTML UTF8) and temporary file cleanup
 $ReportHTMLFile | clip
 Invoke-Item $ReportHTMLFile
 # Remove-Item $ReportDataFile
-# Remove-Item $ReportErrorFile
+Remove-Item $ReportErrorFile
